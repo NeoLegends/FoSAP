fa596fa217efc47a62132a9f93c496ce1c5eaf92
Moritz Gunz <moritz.gunz@gmail.com>
2017-05-29 20:25:06 +0200

    :memo: Add more docs

--- H8-nfa/src/nfa.rs
+++ H8-nfa/src/nfa.rs
@@ -15,7 +15,13 @@ pub struct NFA<S, A> {
 impl<S, A> NFA<S, A>
         where S: Clone + Eq + Hash,
               A: Eq + Hash {
-    /// Creates a new NFA with the given states.
+    // Vorwort:
+    //
+    // Lässt man in Rustlang das Semikolon am Ende eines Ausdrucks weg,
+    // wird er returnt. Idiomatischer Rust-Code nutzt nur sehr wenige
+    // explizite returns.
+
+    /// Creates a new NFA.
     pub fn new() -> NFA<S, A> {
         NFA {
             transitions: HashMap::new()
@@ -36,13 +42,18 @@ impl<S, A> NFA<S, A>
 
     /// Simulates the NFA from the given starting point.
     pub fn simulate(&self, from: S, with: &[A]) -> HashSet<S> {
-        // Wir bauen uns ein Set mit den Zuständen, in denen der Automat
-        // aktuell sein kann. Anfangs ist es nur der gegebene Startzustand.
+        // Als Initialwert für unten nehmen wir den Startzustand, von dem wir
+        // aus simulieren sollen.
         let mut start_state = HashSet::new();
         start_state.insert(from);
 
-        // .fold ist eine Akkumulator-Funktion, durch die alle Buchstaben aus
-        // dem gegebenen Wort in die möglichen Zustände "reduziert" werden.
+        // .fold ist eine Akkumulator-Funktion, durch die alle Buchstaben der
+        // Eingabe in die möglichen Zustände "reduziert" werden.
+        //
+        // Es gibt einen Initialzustand und dann eine Funktion, die den jeweiligen
+        // aktuellen Zustand und das aktuelle Element des Iterators zum jeweils
+        // neuen Zustand reduziert. Dieser wird dann für die nächste Iteration
+        // verwendet, bzw. am Ende returnt, sobald die Auflistung durchlaufen wurde.
         //
         // In anderen Sprachen bekannt als `reduce` oder `aggregate`, etc.
         with.into_iter()
@@ -50,10 +61,11 @@ impl<S, A> NFA<S, A>
                 // Nun gehen wir die möglichen Zustände des Automaten durch.
                 // Wir schauen nach Transitions von unserem aktuellen Zustand
                 // über das aktuelle Wort. Falls wir welche finden, vereinen
-                // wir die Mengen der gefundenen Zustände zu einer großen,
-                // von der wir dann im nächsten `fold`-Schritt wieder ausgehen.
+                // wir mittels `fold` die Mengen der gefundenen Zustände zu einer
+                // neuen großen, von der wir dann im nächsten `fold`-Schritt wieder
+                // ausgehen.
                 //
-                // .filter_map mappt alle Elemente der Auflistung auf einen
+                // .filter_map mappt dabei alle Elemente der Auflistung auf einen
                 // anderen, und filtert dabei direkt die `null`-Werte heraus.
                 // Somit überspringen wir direkt alle Zustände von denen es
                 // keine Transitionen gibt und die Transitionen, die nicht
